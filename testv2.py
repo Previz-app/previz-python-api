73d9bbe95a5b16054f119ae328980ecd34ceea44
Charles Flèche <charles@previz.co>
2017-08-01 15:53:52 +0200

    Update testv2.py

--- testv2.py
+++ testv2.py
@@ -5,13 +5,13 @@ from pprint import pprint
 import previz
 
 API_ROOT = 'http://previz.app/api'
-API_TOKEN = 'glCjaYtTMe5u3D6eOQi4CGJkoHLbOA7pRlhcbJQL5VQZB1ECXY5UskAg4y17'
+API_TOKEN = 'zdvBU8BtPL5cubhlshz0qsYYaT7B0hMuebpQoMijNcXrN2qDc29WLTCxO5pL'
 
-TEAM_UUIDS = ['94071d22-3fd6-47bc-9e5b-b3b9f234c3f5', 'd899cd46-a94d-4b51-b9ce-eb6569df9c8b']
-TEAM_UUID = TEAM_UUIDS[1]
-PROJECT_UUID = 'ee35122e-65ba-4c56-8280-39653a32bf09'
+#TEAM_UUIDS = ['94071d22-3fd6-47bc-9e5b-b3b9f234c3f5', 'd899cd46-a94d-4b51-b9ce-eb6569df9c8b']
+#TEAM_UUID = TEAM_UUIDS[1]
+#PROJECT_UUID = 'ee35122e-65ba-4c56-8280-39653a32bf09'
 
-p = previz.PrevizProject(API_ROOT, API_TOKEN, PROJECT_UUID)
+p = previz.PrevizProject(API_ROOT, API_TOKEN)
 
 #pprint(p.teams()) # currently broken because of ?include=projects
 #pprint(p.teams([]))
@@ -27,4 +27,7 @@ p = previz.PrevizProject(API_ROOT, API_TOKEN, PROJECT_UUID)
 
 #pprint(p.get_all())
 
-p.assets()
+#p.assets()
+
+#pprint(p.plugins())
+pprint(p.updated_plugin('blender', '0.0.8'))

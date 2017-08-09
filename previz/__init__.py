e5101a3bffbd44d8a37833285563153c0dfb7f6e
Charles Flèche <charles@previz.co>
2017-08-10 01:05:57 +0200

    Bump version: 1.0.0 → 1.0.1

--- previz/__init__.py
+++ previz/__init__.py
@@ -11,7 +11,7 @@ import semantic_version
 
 __author__ = 'Previz'
 __contact__ = 'info@previz.co'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 
 def not_implented_in_v2(func):
     def wrapper(*args, **kwargs):
